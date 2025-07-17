38696e4051f2b13464bb6c1800bd335df166d372
TheSilverLeopard <84735009+TheSilverLeopard@users.noreply.github.com>
2025-07-17 22:59:19 +0100

    s

--- Portfolio/next.config.ts
+++ Portfolio/next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
+const repo = 'Portfolio';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/Portfolio' : '',
+  basePath: isProd ? `/${repo}` : '', 
+  assetPrefix: isProd ? `/${repo}` : '', 
+  images: { unoptimized: isProd },
 };
 
 export default nextConfig;
--- Portfolio/src/app/page.tsx
+++ Portfolio/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/Portfolio/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -34,7 +34,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="/Portfolio/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -60,7 +60,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/Portfolio/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -75,7 +75,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/Portfolio/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -90,7 +90,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="/Portfolio/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
